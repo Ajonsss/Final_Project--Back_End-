100c05779bdc1704434930c126c96973f1a8b155
ure23 <lurainevillaranda@gmail.com>
2025-12-15 09:19:35 +0800

    day 3c

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router(); // <--- Capital 'R' is required here
+const router = express.Router(); 
 const authController = require('../controllers/authController');
 
 router.post('/login', authController.login);
